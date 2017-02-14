224527834f60491dacd257377f2f0676726355ce
fanlychie <fanlychie@yeah.net>
2017-02-15 01:27:34 +0800

    运行时异常

---
+++ src/main/java/org/fanlychie/commons/web/exception/RuntimeCastException.java
@@ -0,0 +1,21 @@
+package org.fanlychie.commons.web.exception;
+
+/**
+ * 运行时异常, 它内部包装了真实的非运行时异常对象, 通过 getCause 来取出真实异常
+ *
+ * Created by 范忠云 on 2017/2/15.
+ */
+public class RuntimeCastException extends RuntimeException {
+
+    private Throwable cause;
+
+    public RuntimeCastException(Throwable throwable) {
+        this.cause = throwable;
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return cause;
+    }
+
+}
