92369888d57bdf6bcd651b786222a36db641e4b2
fanlychie <fanlychie@yeah.net>
2017-03-18 22:30:54 +0800

    添加类的描述信息

--- src/main/java/org/fanlychie/commons/web/spring/validator/constraint/Length.java
+++ src/main/java/org/fanlychie/commons/web/spring/validator/constraint/Length.java
@@ -8,6 +8,9 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * 长度, 仅当标注在 {@link java.lang.CharSequence} 类型的属性或方法时进行值的验证
+ */
 @Documented
 @Target({FIELD, METHOD})
 @Retention(RUNTIME)
