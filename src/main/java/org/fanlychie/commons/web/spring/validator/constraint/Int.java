3828051a3c42bf286b0217c32c4524c572523adb
fanlychie <fanlychie@yeah.net>
2017-04-01 15:55:33 +0800

    添加参数验证注解

--- src/main/java/org/fanlychie/commons/web/spring/validator/constraint/Int.java
+++ src/main/java/org/fanlychie/commons/web/spring/validator/constraint/Int.java
@@ -8,7 +8,7 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * 整数, 仅当标注在 byte/Byte, short/Short, int/Int, long/Long 类型的属性时进行值的验证
+ * 整数, 仅当标注在 byte/Byte, short/Short, int/Integer, long/Long 类型的属性时进行值的验证
  */
 @Documented
 @Target(FIELD)
